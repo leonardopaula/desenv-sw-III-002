ba46cce9bee8d965d233045b2fe60c44d1c3ec10
Samuel Locatelli <mukatk@gmail.com>
2017-04-02 14:49:05 -0300

    criação do MVC e front-end do sistema

---
+++ Web/Helpers/4WebHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Web.Helpers
+{
+    public static class _4WebHelper
+    {
+        /// <summary>
+        /// Helper utilizado para inserir um checkbox sem utilizar um hidden auxiliar 
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="name"></param>
+        /// <param name="htmlAttributes"></param>
+        /// <returns></returns>
+        public static MvcHtmlString CheckBoxSimple<TModel>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, bool>> expression, object htmlAttributes)
+        {
+            string checkBoxWithHidden = htmlHelper.CheckBoxFor(expression, htmlAttributes).ToHtmlString().Trim();
+            string pureCheckBox = checkBoxWithHidden.Substring(0, checkBoxWithHidden.IndexOf("<input", 1));
+            return new MvcHtmlString(pureCheckBox);
+        }
+
+        public static MvcHtmlString CheckBoxSimple(this HtmlHelper htmlHelper, string name, object htmlAttributes)
+        {
+            string checkBoxWithHidden = htmlHelper.CheckBox(name, htmlAttributes).ToHtmlString().Trim();
+            string pureCheckBox = checkBoxWithHidden.Substring(0, checkBoxWithHidden.IndexOf("<input", 1));
+            return new MvcHtmlString(pureCheckBox);
+        }
+
+        public static MvcHtmlString ListBoxSimple<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, IEnumerable<SelectListItem> selectList, string defaultValue, object htmlAttributes)
+        {
+            IEnumerable<SelectListItem> selectDefault = new SelectListItem[] { new SelectListItem { Value = "", Text = defaultValue, Disabled = true } };
+            selectDefault = selectDefault.Concat(selectList);
+            return htmlHelper.ListBoxFor(expression, selectDefault, htmlAttributes);
+        }
+    }
+}
---
+++ Web/Scripts/Site.js
@@ -0,0 +1,4 @@
+﻿$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    $('select').material_select();
+});
